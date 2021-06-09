2d2b722a8459cffb936cc730a728cb692ff41607
Deepak kumar dash <deepakkumardash313@gmail.com>
2021-06-09 16:26:53 +0530

    add register form

--- pages/register.js
+++ pages/register.js
@@ -1,12 +1,59 @@
 /** @format */
 
+import { useState } from "react";
+
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.table({ name, email, password });
+  };
   return (
-    <div className="p-5 mb-4 bg-primary bg-gradient">
-      <div className="container-fluid py-5">
-        <h1 className="text-center text-white">Register</h1>
+    <>
+      <div className="p-5 mb-4 bg-primary bg-gradient">
+        <div className="container-fluid py-5">
+          <h1 className="text-center text-white">Register</h1>
+        </div>
       </div>
-    </div>
+
+      <div className="container col-md-4 offset-md-4 pb-5">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="form-control mb-4 p-3"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Enter your name"
+            required
+          />
+          <input
+            type="email"
+            className="form-control mb-4 p-3"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter your email"
+            required
+          />
+          <input
+            type="password"
+            className="form-control mb-4 p-3"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter your password"
+            required
+          />
+
+          <div className="d-grid gap-2">
+            <button type="submit" className="btn btn-primary ">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
   );
 };
 
