84c952e36d7e3944634536b24f896a2a4f217719
Deepak kumar dash <deepakkumardash313@gmail.com>
2021-06-11 00:17:23 +0530

    add : loading state

--- pages/register.js
+++ pages/register.js
@@ -3,23 +3,28 @@
 import { useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
+import { SyncOutlined } from "@ant-design/icons";
 
 const Register = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
-      e.preventDefault();
+      setLoading(true);
       const { data } = await axios.post(`http://localhost:8000/api/register`, {
         name,
         email,
         password,
       });
       toast.success("Registration Successfull. Please login to continue");
+      setLoading(false);
     } catch (error) {
       toast.error(error.response.data);
+      setLoading(false);
     }
   };
   return (
@@ -58,8 +63,12 @@ const Register = () => {
           />
 
           <div className="d-grid gap-2">
-            <button type="submit" className="btn btn-primary ">
-              Submit
+            <button
+              type="submit"
+              className="btn btn-primary "
+              disabled={loading}
+            >
+              {loading ? <SyncOutlined spin /> : "Submit"}
             </button>
           </div>
         </form>
