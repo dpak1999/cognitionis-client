ad00b4bfae615003de5d86f2303d53a3be16a4f8
Deepak kumar dash <deepakkumardash313@gmail.com>
2021-06-12 23:46:00 +0530

    add : login ui

--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,76 @@
 /** @format */
 
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { SyncOutlined } from "@ant-design/icons";
+import Link from "next/link";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const { data } = await axios.post(`/api/login`, {
+        email,
+        password,
+      });
+      // toast.success("Registration Successfull. Please login to continue");
+      // setLoading(false);
+      console.log(data);
+    } catch (error) {
+      toast.error(error.response.data);
+      setLoading(false);
+    }
+  };
   return (
-    <div className="p-5 mb-4 bg-primary bg-gradient">
-      <div className="container-fluid py-5">
-        <h1 className="text-center text-white">Login</h1>
+    <>
+      <div className="p-5 mb-4 bg-primary bg-gradient">
+        <div className="container-fluid py-5">
+          <h1 className="text-center text-white">Login</h1>
+        </div>
       </div>
-    </div>
+      <div className="container col-md-4 offset-md-4 pb-5">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            className="form-control mb-4 p-3"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter your email"
+            autoComplete="off"
+          />
+          <input
+            type="password"
+            className="form-control mb-4 p-3"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter your password"
+            autoComplete="off"
+          />
+
+          <div className="d-grid gap-2">
+            <button
+              type="submit"
+              className="btn btn-primary "
+              disabled={loading}
+            >
+              {loading ? <SyncOutlined spin /> : "Submit"}
+            </button>
+          </div>
+        </form>
+        <p className="text-center p-3">
+          Don't have an account?{" "}
+          <Link href="/register">
+            <a>Register</a>
+          </Link>
+        </p>
+      </div>
+    </>
   );
 };
 
