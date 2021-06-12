9675402eab45741833885a75a21f2367c912d586
Deepak kumar dash <deepakkumardash313@gmail.com>
2021-06-12 23:57:29 +0530

    setup context for global state management

--- pages/_app.js
+++ pages/_app.js
@@ -6,14 +6,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "antd/dist/antd.css";
 import "../public/css/styles.css";
 import TopNav from "../components/TopNav";
+import { Provider } from "../context";
 
 function App({ Component, pageProps }) {
   return (
-    <>
+    <Provider>
       <ToastContainer position="top-right" />
       <TopNav />
       <Component {...pageProps} />
-    </>
+    </Provider>
   );
 }
 
