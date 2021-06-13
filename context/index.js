c09d04b7c8e3a28a3994c386dbb58165ec9b4716
Deepak kumar dash <deepakkumardash313@gmail.com>
2021-06-13 20:23:37 +0530

    add axios interceptors

--- context/index.js
+++ context/index.js
@@ -1,6 +1,8 @@
 /** @format */
 
+import axios from "axios";
 import { useReducer, createContext, useEffect } from "react";
+import { useRouter } from "next/router";
 
 // Initial state
 const initialState = {
@@ -25,12 +27,47 @@ const rootReducer = (state, action) => {
 // context provider
 const Provider = ({ children }) => {
   const [state, dispatch] = useReducer(rootReducer, initialState);
+  const router = useRouter();
 
   useEffect(() => {
     dispatch({
       type: "LOGIN",
       payload: JSON.parse(window.localStorage.getItem("user")),
     });
+  }, []);
+
+  axios.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    function (error) {
+      let res = error.response;
+      if (res === 401 && res.config && !res.config.__isRetryRequest) {
+        return new Promise((resolve, reject) => {
+          axios
+            .get("/api/logout")
+            .then((data) => {
+              dispatch({ type: "LOGOUT" });
+              window.localStorage.removeItem("user");
+              router.push("/login");
+            })
+            .catch((err) => {
+              console.log(err);
+              reject(error);
+            });
+        });
+      }
+      return Promise.reject(error);
+    }
+  );
+
+  useEffect(() => {
+    const getCsrfToken = async () => {
+      const { data } = await axios.get("/api/csrf-token");
+      axios.defaults.headers["X-CSRF-Token"] = data.getCsrfToken;
+    };
+
+    getCsrfToken();
   }, []);
 
   return (
