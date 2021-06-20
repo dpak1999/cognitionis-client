1aaecc4af0909c214a090454c03bfbfc4ba825a9
Deepak kumar dash <deepakkumardash313@gmail.com>
2021-06-20 20:46:15 +0530

    add : creat course & become instructor page

---
+++ pages/user/become-instructor.js
@@ -0,0 +1,17 @@
+/** @format */
+
+import React from "react";
+
+const BecomeInstructor = () => {
+  return (
+    <>
+      <div className="p-5 mb-4 bg-primary bg-gradient">
+        <div className="container-fluid py-5">
+          <h1 className="text-center text-white">Become Instructor</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BecomeInstructor;
