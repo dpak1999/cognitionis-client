dd00aa547713d7fcd9f60959a9fd3b2479354aca
dpak1999 <deepakkumardash313@gmail.com>
2021-07-25 21:01:10 +0530

    create instructor route and home page

---
+++ components/routes/InstructorRoute.js
@@ -0,0 +1,48 @@
+/** @format */
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { SyncOutlined } from "@ant-design/icons";
+import UserNav from "../nav/UserNav";
+
+const InstructorRoute = ({ children }) => {
+  const [ok, setOk] = useState(false);
+  const router = useRouter();
+
+  const fetchInstructor = async () => {
+    try {
+      const { data } = await axios.get("/api/current-instructor");
+      if (data.ok) setOk(true);
+    } catch (error) {
+      setOk(false);
+      router.push("/");
+    }
+  };
+
+  useEffect(() => {
+    fetchInstructor();
+  }, []);
+
+  return (
+    <>
+      {!ok ? (
+        <SyncOutlined
+          spin
+          className="d-flex justify-content-center p-5 text-primary display-1"
+        />
+      ) : (
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-2">
+              <UserNav />
+            </div>
+            <div className="col-md-10">{children}</div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default InstructorRoute;
--- pages/instructor/course/create.js
+++ pages/instructor/course/create.js
@@ -1,16 +1,17 @@
 /** @format */
 
 import React from "react";
+import InstructorRoute from "../../../components/routes/InstructorRoute";
 
 const CourseCreate = () => {
   return (
-    <>
+    <InstructorRoute>
       <div className="p-5 mb-4 bg-primary bg-gradient">
         <div className="container-fluid py-5">
           <h1 className="text-center text-white">Create course</h1>
         </div>
       </div>
-    </>
+    </InstructorRoute>
   );
 };
 
