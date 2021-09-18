58142686095354f0d45e1958c9f87e2870c99143
dpak1999 <deepakkumardash313@gmail.com>
2021-09-19 04:53:04 +0530

    add : modal to add lesson

--- pages/instructor/course/view/[slug].js
+++ pages/instructor/course/view/[slug].js
@@ -1,15 +1,16 @@
 /** @format */
 
-import axios from "axios";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { Avatar, Tooltip } from "antd";
-import { CheckOutlined, EditOutlined } from "@ant-design/icons";
-import ReactMarkdown from "react-markdown";
-import InstructorRoute from "../../../../components/routes/InstructorRoute";
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { Avatar, Button, Modal, Tooltip } from 'antd';
+import { CheckOutlined, EditOutlined, UploadOutlined } from '@ant-design/icons';
+import ReactMarkdown from 'react-markdown';
+import InstructorRoute from '../../../../components/routes/InstructorRoute';
 
 const CourseView = () => {
   const [course, setCourse] = useState({});
+  const [visible, setVisible] = useState(false);
 
   const router = useRouter();
   const { slug } = router.query;
@@ -31,17 +32,17 @@ const CourseView = () => {
             <div className="d-flex pt-2">
               <Avatar
                 size={80}
-                src={course.image ? course.image.Location : "/def.jpg"}
+                src={course.image ? course.image.Location : '/def.jpg'}
               />
 
               <div className="flex-grow-1 ps-2">
                 <div className="row">
                   <div className="col">
                     <h5 className="mt-2 text-primary">{course.name}</h5>
-                    <p style={{ marginTop: "-10px" }}>
+                    <p style={{ marginTop: '-10px' }}>
                       {course.lessons && course.lessons.length} Lessons
                     </p>
-                    <p style={{ marginTop: "-15px", fontSize: "10px" }}>
+                    <p style={{ marginTop: '-15px', fontSize: '10px' }}>
                       {course.category}
                     </p>
                   </div>
@@ -64,6 +65,28 @@ const CourseView = () => {
                 <ReactMarkdown children={course.description} />
               </div>
             </div>
+            <div className="row">
+              <Button
+                className="col-md-6 offset-md-3 text-center"
+                type="primary"
+                shape="round"
+                icon={<UploadOutlined />}
+                size="large"
+                onClick={() => setVisible(true)}
+              >
+                Add Lesson
+              </Button>
+            </div>
+            <br />
+            <Modal
+              title="+ Add Lesson"
+              centered
+              visible={visible}
+              onCancel={() => setVisible(false)}
+              footer={null}
+            >
+              add lesson coming soon
+            </Modal>
           </div>
         )}
       </div>
