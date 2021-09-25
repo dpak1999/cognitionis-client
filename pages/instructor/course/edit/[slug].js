344b595c477785cd1dc3a13b1649236a6ae00e04
dpak1999 <deepakkumardash313@gmail.com>
2021-09-26 05:28:33 +0530

    handle update lesson

--- pages/instructor/course/edit/[slug].js
+++ pages/instructor/course/edit/[slug].js
@@ -169,8 +169,22 @@ const CourseEdit = () => {
     setUploading(false);
   };
 
-  const handleUpdateLesson = () => {
-    console.log('handle update lesson');
+  const handleUpdateLesson = async (e) => {
+    e.preventDefault();
+    const { data } = await axios.put(
+      `/api/course/lesson/${slug}/${current._id}`,
+      current
+    );
+    setUploadVideoButtonText('Upload Video');
+    setVisible(false);
+
+    if (data.ok) {
+      let arr = values.lessons;
+      const index = arr.findIndex((el) => el._id === current._id);
+      arr[index] = current;
+      setValues({ ...values, lessons: arr });
+      toast('Lesson updated');
+    }
   };
 
   return (
