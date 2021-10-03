938fd4a5f8591f294c074e0fe36a1d8f1c377cb3
dpak1999 <deepakkumardash313@gmail.com>
2021-10-03 23:03:28 +0530

    add : course card

---
+++ components/cards/CourseCard.js
@@ -0,0 +1,37 @@
+/** @format */
+
+import { Badge, Card } from 'antd';
+import Link from 'next/link';
+
+const CourseCard = ({ course }) => {
+  const { slug, name, instructor, price, image, paid, category } = course;
+
+  return (
+    <Link href={`/course/${slug}`}>
+      <a>
+        <Card
+          className="mb-4"
+          cover={
+            <img
+              src={image.Location}
+              alt={name}
+              style={{ height: '200px', objectFit: 'cover' }}
+              className="p-1"
+            />
+          }
+        >
+          <h2 className="font-weight-bold">{name}</h2>
+          <p>By {instructor.name}</p>
+          <Badge
+            count={category}
+            style={{ backgroundColor: '#03a9f4' }}
+            className="me-2 pb-2"
+          />
+          <h4 className="pt-2">{paid ? `$ ${price}` : 'Free'}</h4>
+        </Card>
+      </a>
+    </Link>
+  );
+};
+
+export default CourseCard;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,35 @@
 /** @format */
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import CourseCard from '../components/cards/CourseCard';
 
 const Index = () => {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const { data } = await axios.get('/api/courses');
+      setCourses(data);
+    };
+
+    fetchCourses();
+  }, []);
+
   return (
     <>
       <div className="p-5 mb-4 bg-primary bg-gradient">
         <div className="container-fluid py-5">
           <h1 className="text-center text-white">Cognitionis</h1>
+        </div>
+      </div>
+
+      <div className="container-fluid">
+        <div className="row">
+          {courses.map((course) => (
+            <div key={course._id} className="col-md-4">
+              <CourseCard course={course} />
+            </div>
+          ))}
         </div>
       </div>
     </>
